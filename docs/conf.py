47392bd1f5640eb6f5526d84b38d5b776b698d95
DeastinY <poilzin.richard@gmail.com>
2023-02-16 16:05:32 +0100

    added autodoc

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,12 @@ author = 'Richard Polzin'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
