8fddfc2721e367698c0577d82bcc65e858f54754
Richard Polzin <richard.polzin@posteo.de>
2023-03-10 13:05:15 +0100

    further work docs to fix built on RTD

--- docs/conf.py
+++ docs/conf.py
@@ -1,9 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath("."))
-sys.path.insert(0, os.path.abspath("../"))
-sys.path.insert(0, os.path.abspath(os.path.join('..', 'dea')))
-sys.path.append(os.path.join(os.path.dirname(__name__), '..'))
+sys.path.insert(0, os.path.abspath("../dea/"))
 
 # Configuration file for the Sphinx documentation builder.
 #
